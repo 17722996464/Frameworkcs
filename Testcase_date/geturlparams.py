c234db66c4e0a4cd5a3bba7b42d22fba00bf0d2f
zhouzhou <jol@163.com>
2019-07-31 21:20:31 +0800

    添加备注

--- Testcase_date/geturlparams.py
+++ Testcase_date/geturlparams.py
@@ -1,7 +1,7 @@
 from Testcase_date.readConfig import ReadConfig
 readConfig = ReadConfig()
 class geturlParmes():
-    def get_url(self):     #定义一个方法，读取配置文件进行拼接
+    def get_url(self):     ##定义一个方法，读取配置文件进行拼接
         new_url= readConfig.get_http('scheme') + '://'+readConfig.get_http('baseurl')+':8888'+'/login'+'?'
         return new_url
 if __name__ == '__main__':
