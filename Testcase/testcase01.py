2cbf648f7944fff8eaa2ed4ae38fdd2a3c2a5482
zhouzhou <jol@163.com>
2019-07-31 20:57:26 +0800

    添加备注

--- Testcase/testcase01.py
+++ Testcase/testcase01.py
@@ -5,7 +5,7 @@ import paramunittest
 from Testcase_date.geturlparams import geturlParmes
 import urllib.parse
 from Testcase_date.readExcel import readExcel
-url = geturlParmes().get_url()
+url = geturlParmes().get_url()# 调用我们的geturlParams获取我们拼接的URL
 #print(url)
 login_xls = readExcel().get_xls('zj.xlsx','ww')
 @paramunittest.parametrized(*login_xls)
@@ -24,15 +24,15 @@ class testUserLogin(unittest.TestCase):
         self.checkResult()
     def tearnDown(self):
         print('测试结束，输出log完结\n\n')
-    def checkResult(self):
+    def checkResult(self): #断言
         #url1=geturlParams().get_url('new_url')
         new_url1=url+self.query
-        data1 = dict(urllib.parse.parse_qsl(urllib.parse.urlsplit(new_url1).query))
+        data1 = dict(urllib.parse.parse_qsl(urllib.parse.urlsplit(new_url1).query))# 将一个完整的URL中的name=&pwd=转换为{'name':'xxx','pwd':'bbb'}
          #print(dict.get('name'))
-        info= RunMain().run_main(self.method,url,data1)
-        ss=json.loads(info)
+        info= RunMain().run_main(self.method,url,data1)# 根据Excel中的method调用run_main来进行requests请求，并拿到响应
+        ss=json.loads(info)# 将响应转换为字典格式
         if self.case_name=='login':
-             self.assertEqual(ss['code'],200)
+             self.assertEqual(ss['code'],200)# 如果case_name是login，说明合法，返回的code应该为200
         if self.case_name == 'login_error':
              self.assertEqual(ss['code'],-1)
         if self.case_name=='login_null':
